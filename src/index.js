a3974833dd8f5a1d19b6a2aa8d983c9ddb1db17b
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-03-31 14:33:31 +0530

    axios interceptors

--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+import axios from 'axios';
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, err => {
+    console.log(err);
+    return Promise.reject(err);
+})
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, err => {
+    console.log(err);
+    return Promise.reject(err);
+})
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
